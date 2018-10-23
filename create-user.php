4686bab89a62165d3231acce01d38de50c28ddcf
RAKOTOSOA Feno Zo Tahina <zo.tahina@eufonie.fr>
2018-10-23 16:51:23 +0200

    finalisation d'une ajout des données sur la base de données nomée pool  à l'aide de l'entité User.php

---
+++ create-user.php
@@ -0,0 +1,23 @@
+<?php
+# create-user.php 
+
+$entityManager  = require_once join(DIRECTORY_SEPARATOR,[__DIR__,'config','bootstrap.php']);
+
+use Tuto\Entity\User;
+
+
+// Instanciation de l'utilisateur
+
+$admin = new User();
+
+$admin->setFirstname("First");
+$admin->setLastname("Last");
+$admin->setRole("admin");
+
+
+// Gestion de la persistance
+$entityManager->persist($admin);
+$entityManager->flush();
+
+//vérification du résultats
+echo "Identifiant de l'utilisateur créé : ", $admin->getId();
