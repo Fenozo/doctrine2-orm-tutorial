bd4886e6c566d135c9d7f6d5b585e726bda3ee14
RAKOTOSOA Feno Zo Tahina <960gd7@gmail.com>
2018-10-25 10:02:22 +0200

    create get-poll.php

---
+++ web/view/ManyToOne/get-poll.php
@@ -0,0 +1,22 @@
+<?php
+# get-poll.php
+
+$entityManager = require_once join(DIRECTORY_SEPARATOR, [__DIR__,"..","..","..","config", 'bootstrap.php']);
+
+use Tuto\Entity\Poll;
+
+$pollRepo = $entityManager->getRepository(Poll::class);
+
+$poll = $pollRepo->find(2);
+
+//echo $poll;exit;
+
+if (!empty($poll )) {
+    foreach ($poll->getQuestions() as $question) {
+        echo "- ", $question;
+    
+        foreach ($question->getAnswers() as $answer) {
+            echo "-- ", $answer;
+        }
+    }
+}
